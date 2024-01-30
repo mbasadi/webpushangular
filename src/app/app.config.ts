111cf5ab7ced6769af2a07965bd3692be450d78a
mbasadi <asadi.mohammadbagher@gmail.com>
2024-01-30 12:46:22 -0500

    how about this

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideRouter(routes), provideServiceWorker('notificationapi-service-worker-0697cc83d566c337f10fdfafdd4c765bnotificationapi-service-worker-0697cc83d566c337f10fdfafdd4c765b.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
